5fab74caf3a179daeeba50e67a7ab78985807eac
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-31 00:09:40 +0100

    making UserName Required

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -13,6 +13,8 @@ namespace InventoryLoginDemo.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Required]
         public int UserId { get; set; }
+
+        [Required]
         public string UserName { get; set; }
         public string Password { get; set; }
     }
